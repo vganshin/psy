da0fab207c95fa0f3326ca1719422d3ed792517d
Vladislav Ganshin <ganshinv@gmail.com>
2015-11-08 20:53:14 +0300

    Good

--- main.js
+++ main.js
@@ -4,6 +4,41 @@ var rules = document.getElementById("rules");
 var start_test = document.getElementById("start_test");
 var submit = document.getElementById("submit");
 
+// == Functions ===
+
+var stopWatch = {
+  getTime: function () {
+    return +new Date()
+  },
+  start: function () {
+    this.startTime = this.getTime();
+  },
+  stop: function () {
+    return this.getTime() - this.startTime;
+  },
+};
+
+var pressPermission = {
+  isAllowedPress: false,
+  allowPress: function () { this.isAllowedPress = true; },
+  diallowPress: function () { this.isAllowedPress = false; },
+};
+
+function timer(callback, delay) {
+  timer.clear();
+  timer.current = setTimeout(callback, delay);
+};
+timer.clear = () => clearTimeout(timer.current);
+
+var nrand = (a, b) => Math.round(a + Math.random() * (b - a));
+
+function keypressListener(handler) {
+  keypressListener.unset();
+  keypressListener.handler = handler;
+  addEventListener("keypress", keypressListener.handler)
+}
+keypressListener.unset = () => removeEventListener("keypress", keypressListener.handler);
+
 // === Settings ===
 
 var settings = {
@@ -14,87 +49,6 @@ var settings = {
 };
 
 // ==== Code ===
-
-var globalState = {
-  REGISTRATION: "REGISTRATION",
-  SHOW_RULES: "SHOW_RULES",
-  TEST: "TEST",
-  EXPERIMENT: "EXPERIMENT",
-  SETTINGS: "SETTINGS",
-  END: "END",
-
-  state: null,
-  setState: function (state) { this.state = state; resolveGlobalState(); },
-  resolve: function () {
-    switch (state.global) {
-      case globalState.REGISTRATION:
-        form.style.display = "block";
-
-        this.next = function () { this.setState(globalState.SHOW_RULES); };
-        break;
-      case globalState.SHOW_RULES:
-        // save user
-        form.style.display = "none";
-        rules.style.display = "block";
-
-        this.next = function () { this.setState(globalState.TEST); };
-        break;
-      case globalState.TEST:
-        rules.style.display = "none";
-        gameField.showMessage(settings.message[0]);
-        seriesState.start();
-
-        this.next = function () { this.setState(globalState.EXPERIMENT); };
-        break;
-      case globalState.EXPERIMENT:
-
-        break;
-      case globalState.SETTINGS:
-
-        break;
-      case globalState.END:
-
-        break;
-    }
-  },
-  next: function () {},
-}
-
-var experimentState = {
-  state: experimentState.PREPARE,
-  setState: function (state) { this.state = state; resolveExperimentState(); },
-  PREPARE: "PREPARE",
-  ALLOW_PRESS: "ALLOW_PRESS",
-  END_EXPERIMENT: "END_EXPERIMENT",
-  ERROR: "ERROR"
-}
-
-function resolveExperimentState () {
-
-}
-
-function nextExperimentState () {
-  switch (experimentState.global) {
-    case null:
-      globalState.setState(globalState.REGISTRATION);
-      break;
-    case globalState.REGISTRATION:
-      globalState.setState(globalState.SHOW_RULES);
-      break;
-    case globalState.SHOW_RULES:
-      globalState.setState(globalState.TEST);
-      break;
-    case globalState.TEST:
-      globalState.setState(globalState.EXPERIMENT);
-      break;
-    case globalState.EXPERIMENT:
-      globalState.setState(globalState.END);
-      break;
-    case globalState.END:
-      globalState.setState(globalState.REGISTRATION);
-      break;
-  }
-}
 
 var gameField = {
   dom: document.getElementById("circle"),
@@ -117,101 +71,71 @@ var gameField = {
 
 form.style.display = "block";
 submit.onclick = () => {
-  nextGlobalState();
+  form.style.display = "none";
+  rules.style.display = "block";
 }
 start_test.onclick = () => {
-  nextGlobalState();
+  rules.style.display = "none";
+  prepareExperiment();
 }
 
-var stopWatch = {
-  getTime: function () {
-    return +new Date()
-  },
-  start: function () {
-    this.startTime = this.getTime();
-  },
-  stop: function () {
-    return this.getTime() - this.startTime;
-  },
-};
-
-var pressPermission = {
-  isAllowedPress: false,
-  allowPress: function () { this.isAllowedPress = true; },
-  diallowPress: function () { this.isAllowedPress = false; },
-};
-
-var gameState = {
-  gameStarted: false,
-  startGame: function ( ) { this.gameStarted = true; },
-  endGame: function ( ) { this.gameStarted = false; },
+function prepareExperiment() {
+  gameField.showMessage(settings.message[0])
+  keypressListener((e) => {
+    if (e.charCode === 32) {
+      startExperiment();
+    }
+  })
 }
 
-var seriesState = {
-  ready: false,
-  start: function ( ) { this.ready = true; },
-  end: function ( ) { this.ready = false; },
+function startExperiment() {
+  keypressListener(resolve_space);
+  showExperiment();
 }
 
-function timer(callback, delay) {
-  timer.clear();
-  timer.current = setTimeout(callback, delay);
-};
-timer.clear = () => clearTimeout(timer.current);
+function endExperiment() {
+  settings.attentionTime.shift();
+  settings.numberOfExperiments.shift();
+  settings.message.shift();
 
-var nrand = (a, b) => Math.round(a + Math.random() * (b - a));
+  // save results
+
+  results = [];
+  timer(() => {
+    if (settings.message[0]) {
+      prepareExperiment();
+    } else {
+      keypressListener.unset();
+      gameField.showMessage("Game Over");
+    }
+  }, 1000)
+  
+}
 
 var results = [];
 
-addEventListener("keypress", e => {
+function resolve_space(e) {
   var result = stopWatch.stop();
-  if (e.charCode === 32) {
-    if (gameState.gameStarted) {
-      gameState.endGame();
-      timer.clear();
-      if (pressPermission.isAllowedPress) {
-        gameField.showMessage(result);
-        saveResult(result);
-      } else {
-          gameField.showMessage("Error");
-          showExperiment();
-      }
-    } else if (seriesState.ready) {
-      gameField.showMessage("");
-      seriesState.end();
-      showExperiment();
+  timer.clear();
+  if (e.charCode === 32 && pressPermission.isAllowedPress) {
+    gameField.showMessage(result);
+    results.push(result);
+    if (results.length % settings.numberOfExperiments[0] === 0) {
+      endExperiment();
+    } else {
+      timer(() => showExperiment(), 1000);
     }
-  }
-});
-
-function saveResult(result) {
-  results.push(result)
-  if (results.length % settings.numberOfExperiments[0] === 0) {
-    // refactor this
-    settings.attentionTime.shift();
-    settings.numberOfExperiments.shift();
-    settings.message.shift();
-
-    gameField.showMessage(settings.message[0]);
-    seriesState.start();
-    // timer(() => showExperiment(), 1000);
   } else {
-    showExperiment();
+    gameField.showMessage("Error");
+    timer(() => showExperiment(), 1000);
   }
-}
+};
 
-function showExperiment(time) {
+function showExperiment() {
   var attentionTime = settings.attentionTime[0]; // time
   var waitBeforeRed = settings.waitBeforeRed;
   
-  // убрать отсюда
-  if (!attentionTime) {
-    timer(() => gameField.showMessage("Game over!"), 1000);
-    return;
-  } 
-  
   pressPermission.diallowPress();
-  gameState.startGame();
 
   function showAttention() {
     gameField.showMessage("Внимание");
@@ -230,60 +154,6 @@ function showExperiment(time) {
     gameField.showRedCircle();
   }
   
+  gameField.showMessage("");
   return timer(() => showAttention(), 1000);
-}
-
-globalState.setState(globalState.REGISTRATION);
-
-
-
-function resolveGlobalState () {
-  switch (state.global) {
-    case globalState.REGISTRATION:
-      form.style.display = "block";
-      break;
-    case globalState.SHOW_RULES:
-      // save user
-      form.style.display = "none";
-      rules.style.display = "block";
-      break;
-    case globalState.TEST:
-      rules.style.display = "none";
-      gameField.showMessage(settings.message[0]);
-      seriesState.start();
-
-      break;
-    case globalState.EXPERIMENT:
-
-      break;
-    case globalState.SETTINGS:
-
-      break;
-    case globalState.END:
-
-      break;
-  }
-}
-
-function nextGlobalState () {
-  switch (globalState.state) {
-    case null:
-      globalState.setState(globalState.REGISTRATION);
-      break;
-    case globalState.REGISTRATION:
-      globalState.setState(globalState.SHOW_RULES);
-      break;
-    case globalState.SHOW_RULES:
-      globalState.setState(globalState.TEST);
-      break;
-    case globalState.TEST:
-      globalState.setState(globalState.EXPERIMENT);
-      break;
-    case globalState.EXPERIMENT:
-      globalState.setState(globalState.END);
-      break;
-    case globalState.END:
-      globalState.setState(globalState.REGISTRATION);
-      break;
-  }
 }
